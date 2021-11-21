14e665063833d38debb758d676a310ef78b158fa
Alperen Yılmaz <alperenylmaz61@gmail.com>
2021-11-22 00:05:08 +0300

    update commit
    update commit

--- install_tesseract.sh
+++ install_tesseract.sh
@@ -4,6 +4,14 @@ sudo apt install ttf-mscorefonts-installer
 sudo apt install fonts-dejavu
 fc-cache -vf
 
+# Update the machine
+sudo apt-get update
+
+# Install training dependencies
+sudo apt-get install libicu-dev libpango1.0-dev libcairo2-dev -y
+sudo apt install libtesseract-dev -y
+sudo apt-get install libleptonica-dev -y
+
 # Create output folder
 mkdir output
 
@@ -22,9 +30,28 @@ wget https://raw.githubusercontent.com/tesseract-ocr/langdata/main/eng/eng.punc
 wget https://raw.githubusercontent.com/tesseract-ocr/langdata/main/eng/eng.numbers
 wget https://raw.githubusercontent.com/tesseract-ocr/langdata/main/eng/eng.wordlist
 
-# Clone tesseract repo
+# Clone tesseract repo and tesseract
 cd ~/tesseract-fine-tuning
 git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git
+sudo apt install tesseract-ocr
+
+# Install tesseract
+cd tesseract/
+./autogen.sh
+./configure
+make
+sudo make install
+sudo ldconfig
+make training
+sudo make training-install
+
+# Add path to .bashrc
+echo  'export TESSDATA_PREFIX=/usr/local/share/tessdata' >> ~/.bashrc
+source ~/.bashrc
+
+# Testing tesseract installation
+echo `tesseract --version | grep 'tesseract'`
+
 
 # Download tesstrain
 cd tesseract/
